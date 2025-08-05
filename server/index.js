0cc1e9424eaef9ef7e40ac0b5fd8a02c8e912b08
Nilesh <nileshdadab@gmail.com>
2025-08-05 11:57:42 +0530

    Set up server environment variables and establish MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
-// index.js
 const express = require("express");
-const app = express();
-const PORT = 5000;
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
