e8235ad9b5be3ccd98b7fcec5575c30870710b79
Nilesh <nileshdadab@gmail.com>
2025-08-05 11:58:15 +0530

    application added

---
+++ server/models/application.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema(
+  {
+    jobId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    name: String,
+    email: String,
+    resumeUrl: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Application", applicationSchema);
